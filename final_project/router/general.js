c3bcbb41e28560fe7b95b98bf90be6afb9005d2d
clarafuwen <clarafuwen@gmail.com>
2024-04-18 14:25:43 -0400

    Add Promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,8 +4,8 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+
 public_users.post("/register", (req,res) => {
-  //Write your code here
   const username = req.body.username;
   const password = req.body.password;
   const username_valid = isValid(username);
@@ -15,65 +15,78 @@ public_users.post("/register", (req,res) => {
   } else return res.status(404).json({message: "User already exists!"});  
 });
 
+
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
  const result = new Promise((resolve, reject) =>{
-    resolve(books);
+   resolve(res.status(200).send(JSON.stringify({books}, null, 4)));
  }) 
- result.then((booklist) =>{
-    console.log("resolved");
-    return res.status(200).json(booklist);
- })
+ result.then(() => console.log('Task 10 completed'));
 });
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  const isbn = req.params.isbn;
-  //if book with isbn in stock, return the book, else status 204 with no content
-  const book = books[isbn];
-  if (book) {
-    return res.status(200).json(book);
-  } else return res.status(204).json({ message: "no book with isbn: " + isbn + " found.\n" });
+      const result = new Promise((resolve, reject) =>{
+        const isbn = req.params.isbn;
+        const book = books[isbn];
+//if the book with isbn exists, reslove; otherwise reject with error msg
+        if(book){
+            resolve(res.status(200).send(book));
+        }else reject(res.status(304).send({message:"No book with isbn: " + isbn + " found."})) 
+      }) 
+
+  result.then(() => console.log('Task 11 completed')).catch((err) =>{
+    console.log(err)
+  })
  });
   
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  const vals = Object.values(books);
-  const author = req.params.author;
-  let res_books = [];
-  for(let i = 0; i < vals.length;i++){
-    if(vals[i]['author'] === author){
-        res_books.push(vals[i]);
-    }
-  }
-  if(res_books.length>0){
-    return res.status(200).json(res_books);
-  } else return res.status(204).json({message: "no book authored by: " + author + " found.\n"});
-});
 
+// Get book details based on author
+public_users.get('/author/:author', function (req, res) {  
+    const result = new Promise((resolve, reject) =>{
+        const author = req.params.author;
+        const vals = Object.values(books);
+        //get the books authored by "author"
+        let res_books = [];
+        for(let i = 0; i < vals.length;i++){
+            if(vals[i]['author'] === author){
+                res_books.push(vals[i]);
+            }
+  }
+//if the book authored by author exists, reslove; otherwise reject with error msg
+        if(res_books.length > 0){
+            resolve(res.status(200).send(res_books));
+        }else reject(res.status(204).json({message:"No book authored by : " + author + " found."})) 
+      }) 
+
+      result.then(() => console.log('Task 12 completed')).catch((err)=>
+      console.log(err));
+});
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  const title = req.params.title;
-  const vals = Object.values(books);
-  let res_books = [];
-  for(let i = 0; i < vals.length;i++){
-    if(vals[i]['title'] === title){
-        res_books.push(vals[i]);
-    }
-  }
-  if(res_books.length>0){
-    return res.status(200).json(res_books);
-  } else return res.status(204).json({message: "no book with title: " + title + " found.\n"});
+    const result = new Promise((resolve, reject) =>{
+        const title = req.params.title;
+        const vals = Object.values(books);
+        //get the books with title "title"
+        let res_books = [];
+        for(let i = 0; i < vals.length;i++){
+          if(vals[i]['title'] === title){
+              res_books.push(vals[i]);
+          }
+        }
+//if the book with title exsits, resolve, otherwise reject        
+        if(res_books.length>0){
+            resolve(res.status(200).json(res_books));
+        } else reject(res.status(204).json({message: "No book with title: " + title + " found."}));
+    })
+    result.then(() => console.log('Task 13 completed')).catch((err) => 
+    console.log(err))
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
   const isbn = req.params.isbn;
   if(books[isbn]){
     return res.status(200).json(books[isbn]['reviews'])
